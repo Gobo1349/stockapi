6e1c896509c8de550b970d4fd1a0941c9dffdb34
Samsonov Mihail <misamsonov@ozon.ru>
2022-08-26 01:39:25 +0300

    Init commit

---
+++ EduStockApi/src/OzonEduStockApi/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using stockapi;
+
+CreateHostBuilder(args).Build().Run(); // создает хост - все компоненты, в т ч сервер, и конфигурации 
+    
+// метод main генерируется сам 
+
+// задает точку входа в приложение
+static IHostBuilder CreateHostBuilder(string[] args)
+    => Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(
+            webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            }
+        );
---
+++ EduStockApi/src/OzonEduStockApi/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace stockapi 
+{
+    public class Startup // конфигурирует хост - он содержит элементы для обработки запроса
+    {
+        private readonly IConfiguration _configuration;
+        
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        public void ConfigureServices(IServiceCollection services) // конфигурирует DI контейнер
+        {
+            // регистрируем тут сервисы, они будут использоваться при обработке запроса
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app) // регистрируем(?) само приложение - пайплайн обработки запроса 
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {   // эндпоинты определяют то, с чем будет дальше работать приложение 
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
